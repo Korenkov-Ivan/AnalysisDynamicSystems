5d2278a5d4b5f321e01c067960fbfcce786496b5
Korenkov-Ivan <KorenkovIM@gmail.com>
2023-06-21 23:35:20 +0300

    Add ctor ShimizyMoriokaDynamicSystem

--- AnalysisDynamicSystems/ADS.Core/DynamicSystems/ShimizyMoriokaDynamicSystem.cs
+++ AnalysisDynamicSystems/ADS.Core/DynamicSystems/ShimizyMoriokaDynamicSystem.cs
@@ -8,9 +8,25 @@ public class ShimizyMoriokaDynamicSystem: DynamicSystemThree
     public const string
         LAMBDA = "Lambda",
         ALPHA = "Alphs";
+    private const string
+        NAME_DYNAMIC_SYSTEM = "Система Шимицу-Мориока";
+
+    private const float
+        DEFAULT_LAMBDA = 1f,
+        DEFAULT_ALPHA = 0.8f;
 
     public override float Fx(Vector3 v) => v.Y;
     public override float Fy(Vector3 v) => v.X - this[LAMBDA] * v.Y - v.X * v.Z;
     public override float Fz(Vector3 v) => -this[ALPHA] * v.Z + v.X * v.X;
+
+    public ShimizyMoriokaDynamicSystem()
+        :base(NAME_DYNAMIC_SYSTEM)
+    {
+        Parametr = new()
+        {
+            {LAMBDA, DEFAULT_LAMBDA},
+            {ALPHA, DEFAULT_ALPHA},
+        };
+    }
 
 }
