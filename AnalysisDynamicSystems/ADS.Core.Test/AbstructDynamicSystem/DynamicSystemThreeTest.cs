be3c75810a50d9640c74974dd2d2e2dea5714c34
Korenkov-Ivan <KorenkovIM@gmail.com>
2023-06-22 22:28:30 +0300

    fix text for moq

--- AnalysisDynamicSystems/ADS.Core.Test/AbstructDynamicSystem/DynamicSystemThreeTest.cs
+++ AnalysisDynamicSystems/ADS.Core.Test/AbstructDynamicSystem/DynamicSystemThreeTest.cs
@@ -1,5 +1,6 @@
 ﻿using System.Numerics;
 using ADS.Core.AbstructDynamicSystem;
+using Moq;
 
 namespace ADS.Core.Test.AbstructDynamicSystem;
 
@@ -13,30 +14,40 @@ public class DynamicSystemThreeTest
     [Test]
     public void Test1()
     {
-        // Actual
-        var a = new A();
+        // Arrange
+        var moqDynamicSystem = new Mock<DynamicSystemThree>();
+        moqDynamicSystem
+            .Setup(ds => ds.Fx(It.IsAny<Vector3>()))
+            .Returns<Vector3>(r => r.X);
+        moqDynamicSystem
+            .Setup(ds => ds.Fy(It.IsAny<Vector3>()))
+            .Returns<Vector3>(r => r.Y);
+        moqDynamicSystem
+            .Setup(ds => ds.Fz(It.IsAny<Vector3>()))
+            .Returns<Vector3>(r => r.Z);
         var steap = 0.001f;
         var result = new Vector3(1, 1, 1);
+        moqDynamicSystem.Object.MethodIntegration = TypeIntegration.EulerMethod;
 
         // Act
         for (var i = 0; i < 1000; i++)
         {
-            result = a.NextVector(result, steap);
+            result = moqDynamicSystem.Object.NextVector(result, steap);
         }
         Console.WriteLine(result.X);
         // Assert
         Assert.IsTrue(Math.Abs(result.X - Math.E) < 0.01);
     }
 
-    class A : DynamicSystemThree
-    {
-        public override float Fx(Vector3 vector) => vector.X;
-        public override float Fy(Vector3 vector) => vector.Y;
-        public override float Fz(Vector3 vector) => vector.Z;
-
-        public override Vector3 GetDefaultStartVector()
-        {
-            throw new NotImplementedException();
-        }
-    }
+    // class A : DynamicSystemThree
+    // {
+    //     public override float Fx(Vector3 vector) => vector.X;
+    //     public override float Fy(Vector3 vector) => vector.Y;
+    //     public override float Fz(Vector3 vector) => vector.Z;
+    //
+    //     public override Vector3 GetDefaultStartVector()
+    //     {
+    //         throw new NotImplementedException();
+    //     }
+    // }
 }
