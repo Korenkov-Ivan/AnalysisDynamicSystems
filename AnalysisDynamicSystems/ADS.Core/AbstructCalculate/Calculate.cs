dcec4c89f2917e47c3e782f5e53566554f46861b
Korenkov-Ivan <KorenkovIM@gmail.com>
2023-06-22 22:39:30 +0300

    Add Calculate

---
+++ AnalysisDynamicSystems/ADS.Core/AbstructCalculate/Calculate.cs
@@ -0,0 +1,33 @@
+﻿using ADS.Core.AbstructDynamicSystem;
+
+namespace ADS.Core.AbstructCalculate;
+
+/// <summary>
+/// Абстрактный класс для исчислений
+/// </summary>
+/// <typeparam name="TDynamicSystem">Тип динамической системы</typeparam>
+/// <typeparam name="TResult">Тип результата</typeparam>
+/// <typeparam name="TActivate">Тип передаваемых параметров</typeparam>
+public abstract class Calculate<TDynamicSystem, TResult, TActivate>
+{
+    /// <summary>
+    /// Динамическая система
+    /// </summary>
+    public DynamicSystem<TDynamicSystem> CurrentDynamicSystem { get; private set; }
+    
+    public Calculate(DynamicSystem<TDynamicSystem> dynamicSystem)
+    {
+        CurrentDynamicSystem = dynamicSystem;
+    }
+    /// <summary>
+    /// Акт вычисления
+    /// </summary>
+    /// <param name="item">Передаваемые параметры</param>
+    /// <returns>Результат</returns>
+    public abstract TResult Activate(TActivate item);
+    /// <summary>
+    /// Акт вычисления
+    /// </summary>
+    /// <returns>Результат</returns>
+    public abstract TResult Activate();
+}
