a2b28dc9cee93a51cbebb178332c6e87f7ae8ea5
Korenkov-Ivan <KorenkovIM@gmail.com>
2023-06-24 18:44:24 +0300

    Add Lyapynov calculation

--- AnalysisDynamicSystems/ADS.Core/Calculate/LyapynovExponentCalculate.cs
+++ AnalysisDynamicSystems/ADS.Core/Calculate/LyapynovExponentCalculate.cs
@@ -1,4 +1,5 @@
-﻿using ADS.Core.AbstructCalculate;
+﻿using System.Numerics;
+using ADS.Core.AbstructCalculate;
 using ADS.Core.AbstructDynamicSystem;
 
 namespace ADS.Core.Calculate;
@@ -7,10 +8,35 @@ public class LyapynovExponentCalculate: CalculateThree<float, LyapynovExponentPa
 {
     public LyapynovExponentCalculate(DynamicSystemThree dynamicSystem)
         : base(dynamicSystem) { }
-
+    /// <summary>
+    /// Вычисляет старщий ляпуновский показатель
+    /// </summary>
+    /// <param name="item">Параметры для старшего ляпуновского показателя</param>
+    /// <returns>Старший ляпуновский показатель</returns>
     public override float Activate(LyapynovExponentParametrs item)
     {
-        throw new Exception();
+        Vector3
+            vector = new Vector3(0.1f, 0, 0),
+            closeVector = vector;
+        var result = 0d;
+
+        for (int i = 0; i < item.CountIteration; i++)
+        {
+            vector = CurrentDynamicSystem.NextVector(vector, item.Steap);
+            closeVector = CurrentDynamicSystem.NextVector(closeVector, item.Steap);
+
+            var lengthSeparet = (vector - closeVector).Length();
+
+            result += Math.Log(lengthSeparet / item.Eps);
+
+            if (result == double.PositiveInfinity || result == double.NegativeInfinity || result == double.NaN) return float.NaN;
+
+            closeVector.X = vector.X + (closeVector.X - vector.X) / lengthSeparet * item.Eps;
+            closeVector.Y = vector.Y + (closeVector.Y - vector.Y) / lengthSeparet * item.Eps;
+            closeVector.Z = vector.Z + (closeVector.Z - vector.Z) / lengthSeparet * item.Eps;
+        }
+        return (float)result / (item.CountIteration * item.Steap);
+
     }
 }
 
